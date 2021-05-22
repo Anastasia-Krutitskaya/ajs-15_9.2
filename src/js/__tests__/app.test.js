8ec5f9aa1b8a785e82f78816f38db8423f5615ca
Анастасия Крутицкая <anastasia@MacBook-Pro-Anastasia.local>
2021-05-22 07:49:52 +0300

    commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,7 @@
+import ArrayBufferConverter, { getBuffer } from '../app';
+
+test('test', () => {
+  const converter = new ArrayBufferConverter();
+  converter.load(getBuffer());
+  expect(`${converter}`).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+});
